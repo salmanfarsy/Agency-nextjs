fa4e6b75ed61323b7d155b8a1c14f412b3710c74
Salman Farsy <75604210+salmanfarsy@users.noreply.github.com>
2022-04-07 04:16:57 -0700

    nexj files

---
+++ data.js
@@ -0,0 +1,57 @@
+export const data =[
+    {
+        id:0,
+        name:'html',
+        title:'Learn HTML5',
+        desc:'We will teach you the basic of HTML5 and the most common tags you need to remember',
+        photo:'html.jpg'
+    },
+    {
+        id:1,
+        name:'css',
+        title:'Learn CSS3',
+        desc:'We will teach you the basic of CSS3 and the most common properties you need to remember. also with responsive design',
+        photo:'css.png'
+    },
+    {
+        id:2,
+        name:'js',
+        title:'Learn Javascript',
+        desc:'We will teach you the basic of Javascript and learn the basics of programming and build any frontend with application',
+        photo:'js.png'
+    },
+    {
+        id:3,
+        name:'node',
+        title:'Learn NodeJs',
+        desc:'We will teach you the basic of NodeJs and How we can easily build a backend and help you manage the server side of a webside',
+        photo:'node.png'
+    },
+]
+
+export const users = [
+    {
+        id:0,
+        logo:'yahoo.png',
+        avater:'user0.jpg',
+        comment:'They are really good teaching . I enjoyed learning to code with them . And I will also suggest other to come here if they wanna learn to code. They are really sincere with their work',
+        name:'Jack Everton',
+        title:'UI Designer'
+    },
+    {
+        id:1,
+        logo:'google.png',
+        avater:'user1.jpg',
+        comment:'I have learned a lot . But most importantly they helped me to grow and think like a programmer. I can see everything in a new way now . I am always thankful',
+        name:'Alex Durbey',
+        title:'Database Manager'
+    },
+    {
+        id:2,
+        logo:'Facebook.png',
+        avater:'user2.jpg',
+        comment:'It was a wonderful exprience. Learning was really easy and fun . I didnt feel any stressed',
+        name:'Ervin Stock',
+        title:'Backend Engineer '
+    },
+]
